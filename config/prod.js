d51187e546cebaaa1ab0c4c030f458841c09938f
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-01-25 21:27:51 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mogoURI: process.nextTick.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
 
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://mongoUser:0208@projectcluster.eu7fm.mongodb.net/<dbname>?retryWrites=true&w=majority' ,{
+mongoose.connect(config.mongoURI ,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))  
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
